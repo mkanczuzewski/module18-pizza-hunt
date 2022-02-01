4a94ab0340e4c1393c9e72c2ee89e7511a8502e2
mkanczuzewski <mkanczuzewski@gmail.com>
2022-02-01 10:05:02 -0500

    Completed feature branch updates and tested

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -5,22 +5,38 @@ const pizzaController = {
 
     // get all pizzas
     getAllPizza(req, res) {
-        Pizza.find ({})
-        .then(dbPizzaData => res.json(dbPizzaData))
-        .catch(eer => {
-            console.log(err);
-        });
+        Pizza.find({})
+            //in MongoDB instead of joining two tables in mySQL to get data, in MongoDB we populate a field
+            .populate({
+                path: 'comments',
+                //select tells MongoDB we do not care about the __v field on comments. The minus indicates we dont want it to be returned.
+                select: '-__v'
+            })
+            .select('-__v')
+            // Sort so the newest pizza returns first. the minus sorts in DESC order by _id value
+            .sort({ _id: -1 })
+            .then(dbPizzaData => res.json(dbPizzaData))
+            .catch(eer => {
+                console.log(err);
+            });
     },
 
-    //get one pizza by id
+    // get one pizza by id
+    // No sort needed because you will only be sorting by a single pizza.
     getPizzaById({ params }, res) {
         Pizza.findOne({ _id: params.id })
-            .then(dbPizzaData => {
-            // If no Pizza is found, send 404
+        .populate({
+            path: 'comments',
+            select: '-__v'
+        })
+        .select('-__v')
+        .then(dbPizzaData => {
             if (!dbPizzaData) {
-                res.status(404).json({ message: 'No pizza found with this id!' });
-                return;
+            // if no pizza found return 404 error
+            res.status(404).json({ message: 'No pizza found with this id!' });
+            return;
             }
+            // returns back data in dbPizzaData
             res.json(dbPizzaData);
         })
         .catch(err => {
@@ -52,6 +68,7 @@ const pizzaController = {
     // delete pizza
     deletePizza({ params }, res) {
         Pizza.findOneAndDelete({ _id: params.id })
+            // Need to make sure data exists with the if statement. If it skips over this then it found an ID match.
             .then(dbPizzaData => {
                 if (!dbPizzaData) {
                     res.status(404).json({ message: 'No pizza found wiht this id!' });
--- models/Pizza.js
+++ models/Pizza.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
 const PizzaSchema = new Schema({
     pizzaName: {
@@ -9,14 +10,36 @@ const PizzaSchema = new Schema({
     },
     createdAt: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+        //Getters transform data by default everytime it is queried using the dateFormat function that is required above.
+        get: (createdAtVal) => dateFormat(createdAtVal)
     },
     size: {
         type: String,
         default: 'Large'
     },
-    toppings: []
-});
+    toppings: [],
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
+},
+// Tell schema that it can use virtuals like the comment count function below. Also tell it to use getters.
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
+  });
 
 // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
