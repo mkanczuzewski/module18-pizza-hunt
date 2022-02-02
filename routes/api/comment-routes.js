d753fdbd3d2a0b8b0baff60f6d429db7f39ecfb4
mkanczuzewski <mkanczuzewski@gmail.com>
2022-02-02 13:07:07 -0500

    finished reply work

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,10 +1,17 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const {
+    addComment,
+    removeComment,
+    addReply,
+    removeReply
+  } = require('../../controllers/comment-controller');
+ 
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
 
 // /api/comments/<pizzaId>
 router.route('/:pizzaId').post(addComment);
 
 // /api/comments/<pizzaId>/<commentId>
-router.route('/:pizzaId/:commentId').delete(removeComment);
+router.route('/:pizzaId/:commentId').put(addReply).delete(removeComment);
 
 module.exports = router;
